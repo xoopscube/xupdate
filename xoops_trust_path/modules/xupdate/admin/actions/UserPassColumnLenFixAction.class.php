b29f881f7ddb14ee0cce5f104c25416069b7a755
nao-pon <hypweb@gmail.com>
2018-02-23 22:12:08 +0900

    [xupdate] add an function to fixer of XCUBE_CORE_USER_PASS_LEN_FIXED

---
+++ xoops_trust_path/modules/xupdate/admin/actions/UserPassColumnLenFixAction.class.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @file
+ * @package xupdate
+ * @version $Id$
+**/
+
+if (!defined('XOOPS_ROOT_PATH')) {
+    exit;
+}
+
+require_once XUPDATE_TRUST_PATH . '/class/AbstractAction.class.php';
+
+/**
+ * Xupdate_Admin_IndexAction
+**/
+class Xupdate_Admin_UserPassColumnLenFixAction extends Xupdate_AbstractAction
+{
+    /**
+     * getDefaultView
+     * 
+     * @param   void
+     * 
+     * @return  Enum
+    **/
+    public function getDefaultView()
+    {
+        $redirect = xoops_getrequest('xoops_redirect');
+        if ($redirect[0] !== '/') {
+            $redirect = XOOPS_URL . '/index.php';
+        }
+        if (!defined('XCUBE_CORE_USER_PASS_LEN_FIXED') && $this->mod_config['_FtpLoginCheck'] && is_callable('User_Utils::checkUsersPassColumnLength')) {
+            $this->_userPassColumnLenFix();
+        }
+        header('Location: ' . $redirect);
+        exit();
+    }
+
+    private function _userPassColumnLenFix()
+    {
+        if (User_Utils::checkUsersPassColumnLength()) {
+            // update mainfile.php that adds `define('XCUBE_CORE_USER_PASS_LEN_FIXED', true);`
+            if ($this->Ftp->app_login()) {
+                $mainfile = XOOPS_ROOT_PATH . '/mainfile.php';
+                $src = file_get_contents($mainfile);
+                $sens = '/define\s*\(\s*(["\'])XOOPS_MAINFILE_INCLUDED\\1\s*,\s*1\s*\)\s*;\s*(?:\r\n|\n|\r)/';
+                if (preg_match($sens, $src) && !preg_match('/define\s*\(\s*(["\'])XCUBE_CORE_USER_PASS_LEN_FIXED\\1/', $src)) {
+                    $add =  '    // Alrady fixed length of users table pass column of this DB (Auto inserts by X-update)'."\n";
+                    $add .= '    define(\'XCUBE_CORE_USER_PASS_LEN_FIXED\', true);'."\n\n";
+                    $src = preg_replace($sens, '$0' . $add, $src);
+                    $mod = @ fileperms($mainfile);
+                    $this->Ftp->localChmod($mainfile, 0606);
+                    file_put_contents($mainfile, $src, LOCK_EX);
+                    $this->Ftp->localChmod($mainfile, $mod? $mod : 0404);
+                }
+            }
+        }
+    }
+}
--- xoops_trust_path/modules/xupdate/admin/index.php
+++ xoops_trust_path/modules/xupdate/admin/index.php
@@ -6,6 +6,11 @@
  * @version $Id$
 **/
 
+if (!defined('XCUBE_CORE_USER_PASS_LEN_FIXED') && get_class($this) !== 'Xupdate_Admin_UserPassColumnLenFixAction') {
+    header('Location: ' . XOOPS_URL . '/modules/xupdate/admin/index.php?action=UserPassColumnLenFix&xoops_redirect='.rawurlencode($_SERVER['REQUEST_URI']));
+    exit();
+}
+
 $root =& XCube_Root::getSingleton();
 $root->mContext->mModule->setAdminMode(true);
 
